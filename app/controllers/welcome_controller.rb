fae1e049fd4d1fee91428ba15884b6fd61fbac67
Jane Huang <jane.huang@opower.com>
2016-07-04 22:57:29 -0700

    using rspec for testing harness

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    render plain: 'hi'
+    render plain: ':thumbsup:'
   end
 
-  def ssl
-    render plain: 'vKn8WrehP-J-w9psnrcWOaYagOoup3nqR0MyxGoWIK8.akXVc7P_zF82JwQ5zF3fvSawy6xznvLDL9GY3glez6o'
-  end
 end
